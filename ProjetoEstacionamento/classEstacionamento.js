b89c9af90d5a79559563494f634d00516ef9984e
wesleysick <wesley.vasconcelos@sick.com.br>
2023-10-21 13:11:17 -0300

    atualização do projeto 21/10/2023 13:11

---
+++ ProjetoEstacionamento/classEstacionamento.js
@@ -0,0 +1,50 @@
+//CLASSES DO PROJETO ESTACIONAMENTO
+class Estacionamento {
+    constructor() {
+        this.numeroVagasTotais = 100;
+        this.vagasCarro = 80;
+        this.vagasMoto = 20;
+        this.vagasIdosos = 15;
+
+        //Array para monitorar o status das vagas
+        this.statusVagas = Array(this.numeroVagasTotais).fill('Disponível');
+    }
+
+    //Método para verificar o status de uma vaga pelo número
+    verificarStatusVaga(numeroVaga) {
+        if (numeroVaga < 1 || numeroVaga > this.numeroVagasTotais){
+            return 'Número de vaga invalido';
+        }
+
+        return 'A vaga ' + numeroVaga + ' está '+ this.statusVagas[numeroVaga -1];
+    }
+
+    //Método para ocupar uma vaga pelo número
+    ocuparVaga(numeroVaga){
+        if (numeroVaga < 1 || numeroVaga > this.numeroVagasTotais){
+            return 'Número de vaga invalido';
+        }
+
+        if (this.statusVagas[numeroVaga -1] === 'Disponível'){
+            this.statusVagas[numeroVaga -1] = 'Ocupada';
+            return 'A vaga ' + numeroVaga + ' foi ocupada com sucesso!';
+        } else {
+            return 'Vaga já está ocupada!'
+        }
+    }
+
+    liberarVaga(numeroVaga){
+        if (numeroVaga < 1 || numeroVaga > this.numeroVagasTotais){
+            return 'Número de vaga invalido';
+        }
+
+        if (this.statusVagas[numeroVaga -1] === 'Ocupada'){
+            this.statusVagas[numeroVaga -1] = 'Disponível';
+            return  'A vaga ' + numeroVaga +' foi liberada com sucesso!'
+        } else {
+            return 'A vaga ' + numeroVaga + ' já está liberada!'
+        }
+    }
+}
+
+module.exports = Estacionamento;
--- ProjetoEstacionamento/mainCode.js
+++ ProjetoEstacionamento/mainCode.js
@@ -1,63 +1,28 @@
-//PROJETO ESTACIONAMENTO
-class Estacionamento {
-    constructor() {
-        this.numeroVagasTotais = 100;
-        this.vagasCarro = 80;
-        this.vagasMoto = 20;
-        this.vagasIdosos = 15;
+//PROJETO ESTACIONAMENTO main code
 
-        //Array para monitorar o status das vagas
-        this.statusVagas = Array(this.numeroVagasTotais).fill('Disponível');
-    }
-
-    //Método para verificar o status de uma vaga pelo número
-    verificarStatusVaga(numeroVaga) {
-        if (numeroVaga < 1 || numeroVaga > this.numeroVagasTotais){
-            return 'Número de vaga invalido';
-        }
-
-        return this.statusVagas[numeroVaga -1];
-    }
-
-    //Método para ocupar uma vaga pelo número
-    ocuparVaga(numeroVaga){
-        if (numeroVaga < 1 || numeroVaga > this.numeroVagasTotais){
-            return 'Número de vaga invalido';
-        }
-
-        if (this.statusVagas[numeroVaga -1] === 'Disponível'){
-            this.statusVagas[numeroVaga -1] = 'Ocupada';
-            return 'Vaga ocupada com sucesso!';
-        } else {
-            return 'Vaga já está ocupada!'
-        }
-    }
-
-    liberarVaga(numeroVaga){
-        if (numeroVaga < 1 || numeroVaga > this.numeroVagasTotais){
-            return 'Número de vaga invalido';
-        }
-
-        if (this.statusVagas[numeroVaga -1] === 'Ocupada'){
-            this.statusVagas[numeroVaga -1] = 'Disponível';
-            return 'Vaga liberada com sucesso!'
-        } else {
-            return 'A vaga já está liberada!'
-        }
-    }
-}
+const Estacionamento = require('./classEstacionamento.js');
 
 //Criando o objeto do estacionamento
 //Confirmando na tela com console.log()
 const estacionamento = new Estacionamento();
+
+//Mensagem de inicio do sistema
+console.log("Bem-vindo ao sistema de estacionamento WV PARK")
+
+//Imprime na tela o objeto criado
 console.log(estacionamento);
+
 //Consultar disponibilidade da vaga
 console.log(estacionamento.verificarStatusVaga(5));
+
 //Ocupar uma vaga
 console.log(estacionamento.ocuparVaga(5));
+
 //Consultar disponibilidade da vaga
 console.log(estacionamento.verificarStatusVaga(5));
+
 //Liberar uma vaga
 console.log(estacionamento.liberarVaga(5));
+
 //Consultar disponibilidade da vaga
 console.log(estacionamento.verificarStatusVaga(5));
